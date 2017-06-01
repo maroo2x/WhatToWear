fe866a12ac8d90eb3eb848116c42d744251cca06
Marek Kurdziel <marekk-air13@Mareks-MacBook-Air.local>
2017-06-01 21:10:03 +0200

    git remote add origin https://github.com/maroo2x/WhatToWear.git git push -u origin master

---
+++ app/src/main/java/higheye/whattowear/MainActivity.java
@@ -0,0 +1,22 @@
+package higheye.whattowear;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        private GoogleApiClient mGoogleApiClient;
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+}
