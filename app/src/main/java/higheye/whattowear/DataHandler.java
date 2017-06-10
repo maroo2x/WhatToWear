281620015d6c80051aff05d93bfe9d4ae1e652ee
marekk-air13 <kurdzie@gmail.com>
2017-06-10 12:55:42 +0200

    git push -u origin master

--- app/src/main/java/higheye/whattowear/DataHandler.java
+++ app/src/main/java/higheye/whattowear/DataHandler.java
@@ -1,5 +1,9 @@
 package higheye.whattowear;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.widget.ListView;
+
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -7,21 +11,18 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.lang.String;
 
+import static android.R.id.content;
 import static android.R.id.input;
 import static java.security.AccessController.getContext;
 
 /**
  * Created by marekk-air13 on 04/06/2017.
  */
-
 public class DataHandler {
-    static String callback = "";
-
-    public static String Networking(final String address) {
-
+    public static void Networking(final String address, final int type) {
         Thread thread = new Thread(new Runnable() {
             @Override
-            public void run() {
+            public synchronized void run() {
                 try {
                     URL url = new URL(address);
                     HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -34,8 +35,13 @@ public class DataHandler {
                         while ((i = rd.read()) > 0) {
                             response.append((char) i);
                         }
-                        callback = response.toString();
- //
+                        WeatherAdapter weatherAdapter = new WeatherAdapter();
+                        if (type == 0) {
+                            weatherAdapter.setCurrentWeather(response.toString());
+                        } else if (type == 1) {
+                            weatherAdapter.setFutureWeather(response.toString());
+                        }
+                        //
                     } finally {
                         connection.disconnect();
                     }
@@ -45,10 +51,10 @@ public class DataHandler {
             }
         });
         thread.start();
-        return callback;
     }
 
-    public static String getTemperature(String input){
+
+    public static String getTemperature(String input) {
         int index = input.indexOf("\"temp\":");
         if (index == -1) {
             return "no data";
@@ -58,7 +64,7 @@ public class DataHandler {
         return cutString.substring(7, newIndex);
     }
 
-    public static String getClouds(String input){
+    public static String getClouds(String input) {
         int index = input.indexOf("\"all\":");
         if (index == -1) {
             return "no data";
@@ -68,15 +74,23 @@ public class DataHandler {
         return cutString.substring(6, newIndex);
     }
 
-    public static String getIcon(String input){
+    public static String getIcon(String input) {
         int index = input.indexOf("\"icon\":");
         if (index == -1) {
             return "no data";
         }
         String cutString = input.substring(index);
         int newIndex = cutString.indexOf("}");
-        return cutString.substring(8, newIndex-1);
+        return cutString.substring(8, newIndex - 1);
     }
-
 }
 
+/*
+        final Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+        thread.start();
+*/
--- app/src/main/java/higheye/whattowear/MainActivity.java
+++ app/src/main/java/higheye/whattowear/MainActivity.java
@@ -1,15 +1,13 @@
 package higheye.whattowear;
 
 import android.Manifest;
-import android.content.Intent;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Build;
-import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,31 +16,34 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.List;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+import static android.R.attr.delay;
 
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
     TextView mLatitudeText;
     TextView mLongitudeText;
+    TextView weather;
+    TextView weather2;
     GoogleApiClient mGoogleApiClient;
     Location mLastLocation;
-public LocationAdapter locationAdapter = new LocationAdapter();
+    LocationRequest mLocationRequest = LocationRequest.create();
+    public LocationAdapter locationAdapter = new LocationAdapter();
     WeatherAdapter weatherAdapter = new WeatherAdapter();
-    DataHandler dataHandler = new DataHandler();
+    Context context;
+    //    DataHandler dataHandler = new DataHandler(context);
     String latitude;
     String longitude;
-   String currentWeather;
+    String currentWeather;
     String futureWeather;
     static String callback = "";
+    LocationAvailability mLastLocationIsTrue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,12 +51,17 @@ public LocationAdapter locationAdapter = new LocationAdapter();
         setContentView(R.layout.activity_main);
         mLatitudeText = (TextView) findViewById(R.id.mLatitudeText);
         mLongitudeText = (TextView) findViewById(R.id.mLongitudeText);
-        buildGoogleApiClient();
+        weather = (TextView) findViewById(R.id.weather);
+        weather2 = (TextView) findViewById(R.id.weather2);
 
+        // call api client
+        buildGoogleApiClient();
         if (mGoogleApiClient != null) {
             mGoogleApiClient.connect();
-        } else
+        } else {
             Toast.makeText(this, "Not connected...", Toast.LENGTH_SHORT).show();
+        }
+
     }
 
     @Override
@@ -66,30 +72,27 @@ public LocationAdapter locationAdapter = new LocationAdapter();
     //    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onConnected(Bundle arg0) {
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 return;
             }
         }
-
+        //check if current location is available
+//        mLastLocationIsTrue = LocationServices.FusedLocationApi.getLocationAvailability(mGoogleApiClient);
+        // get last location
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-//        LocationServices.FusedLocationApi.requestLocationUpdates;
+        // check if there is any data in current location
         if (mLastLocation != null) {
             latitude = String.valueOf(mLastLocation.getLatitude());
             longitude = String.valueOf(mLastLocation.getLongitude());
-//            cityname = String.valueOf(mLastLocation.get)
-            mLatitudeText.setText("Latitude: " + latitude+", "+"Longitude: " + longitude);
             try {
-                mLongitudeText.setText(getAddress(Double.parseDouble(latitude),Double.parseDouble(longitude)));
+                mLongitudeText.setText(getAddress(Double.parseDouble(latitude), Double.parseDouble(longitude)));
+                locationAdapter.setCoords(latitude, longitude);
+                mLatitudeText.setText("Latitude: " + locationAdapter.getmLatitudeText() + ", " + "Longitude: " + locationAdapter.getmLongitudeText());
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
-//            Toast.makeText(this, "1", Toast.LENGTH_SHORT).show();
-//            currentWeather = DataHandler.Networking("http://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&units=metric&appid=7e7469bd4b8aec9b7684f7b5dd63d3b5");
-            futureWeather = DataHandler.Networking("http://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&units=metric&appid=7e7469bd4b8aec9b7684f7b5dd63d3b5");
-//            Toast.makeText(this, "2", Toast.LENGTH_SHORT).show();
-
         } else {
             mLatitudeText.setText("no current location");
         }
@@ -109,6 +112,7 @@ public LocationAdapter locationAdapter = new LocationAdapter();
         super.onStop();
     }
 
+    // method to build API client
     protected synchronized void buildGoogleApiClient() {
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
@@ -118,20 +122,54 @@ public LocationAdapter locationAdapter = new LocationAdapter();
     }
 
     public void checkResults(View view) {
-        locationAdapter.setCoords(latitude, longitude);
-//        weatherAdapter.setCurrentWeather(currentWeather);
-        weatherAdapter.setFutureWeather(futureWeather);
-//        Toast.makeText(this, weatherAdapter.getCurrentWeather(), Toast.LENGTH_SHORT).show();
-        Intent intent = new Intent(this, ResultsActivity.class);
-        startActivity(intent);
+        determineWeather(0);
+        determineWeather(1);
     }
-    public String getAddress(double latitude, double longitude) throws IOException {
 
+    // method to get and set current weather and forecast
+    public void checkWeather(View view) {
+        determineWeather(0);
+        determineWeather(1);
+    }
+
+    // method to get address basing on latitude and longitude
+    public String getAddress(double latitude, double longitude) throws IOException {
         Geocoder geocoder = new Geocoder(this, Locale.getDefault());
         List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
         String cityName = addresses.get(0).getAddressLine(0);
         String stateName = addresses.get(0).getAddressLine(1);
         String countryName = addresses.get(0).getAddressLine(2);
-        return cityName+", "+stateName; // +", "+countryName;
+        return cityName + ", " + stateName; // +", "+countryName;
     }
+
+    //method to request current location
+    public void requestLocation() {
+        mLocationRequest.setPriority(104);
+    }
+
+
+    public synchronized void determineWeather(int flag) {
+
+        if (flag == 0) {
+            DataHandler.Networking("http://api.openweathermap.org/data/2.5/weather?lat=" + locationAdapter.getmLatitudeText() + "&lon=" + locationAdapter.getmLongitudeText() + "&units=metric&appid=7e7469bd4b8aec9b7684f7b5dd63d3b5", flag);
+            weather.setText(weatherAdapter.getCurrentWeather());
+        } else if (flag == 1) {
+            DataHandler.Networking("http://api.openweathermap.org/data/2.5/forecast?lat=" + locationAdapter.getmLatitudeText() + "&lon=" + locationAdapter.getmLongitudeText() + "&units=metric&appid=06e65c7536988468e72a018ff2e8cf9b", flag);
+            weather2.setText(weatherAdapter.getFutureWeather());
+        }
+    }
+
 }
+
+
+
+
+/*
+        final Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+        thread.start();
+*/
